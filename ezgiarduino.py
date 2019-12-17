914ef35abb45e0cbd3fa8239d0e7f1c53219ef59
ezgialisan <57862637+ezgialisan@users.noreply.github.com>
2019-12-17 20:32:19 +0300

    Add files via upload

---
+++ ezgiarduino.py
@@ -0,0 +1,156 @@
+#include <ros.h>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/Float32.h>
+
+ros::NodeHandle nh; 
+void messageCb(const std_msgs::Float32 &joyinputx, std_msgs::Float32)
+
+    {
+        if (joyinputx.data > 0)
+        {
+            enableforward();
+        }
+        if (joyinputx.data < 0)
+        {
+            enablereverse();
+        }
+        move(255 * abs(joyinputx.data));
+    }
+
+void messageCb(const std_msgs::Float32 &joyinputy, std_msgs::Float32)
+
+     {
+        if (joyinputy.data < 0)
+        {
+              enableright();
+        }
+        if (joinputy.data > 0)
+        {
+             enableleft();
+        }
+          
+        move(255 * abs(joyinputy.data));
+    }
+
+ros::Subscriber<std_msgs::Float32> sub("joyinputy", &messageCb);
+ros::Subscriber<std_msgs::Float32> sub1("joyinputx", &messageCb1);
+
+void setup()
+{
+    for (int i = 8; i <= 13; i++)
+    {
+        pinMode(i, OUTPUT);
+    }
+    for (int i = 48; i <= 53; i++)
+    {
+        pinMode(i, OUTPUT);
+    }
+    for (int i = 2; i <= 7; i++)
+    {
+        pinMode(i, OUTPUT);
+    }
+
+    halt(); //it is for stopping rover
+    nh.initNode();
+    nh.subscribe(sub); //creating subscribers
+    nh.subscribe(sub1);
+    Serial.begin(9600);
+}
+void loop()
+{
+    nh.spinOnce(); //ROS only processes your callbacks when you tell it to with ros::spinOnce()
+    delay(1);
+}
+
+void move(int pwm)
+{
+    for (int i = 2; i <= 7; i++)
+    {
+        analogWrite(i, pwm);
+    }
+}
+
+void halt()
+{
+    for (int k = 2; k <= 7; k++)
+    {
+        analogWrite(k, 0);
+    }
+    for (int i = 48; i <= 53; i++)
+    {
+        digitalWrite(i, LOW);
+    }
+
+    for (int j = 22; j <= 27; j++)
+    {
+        digitalWrite(j, LOW);
+    }
+}
+
+
+void enableRight()
+{
+    digitalWrite(13, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9,  LOW);
+    digitalWrite(53, HIGH);
+    digitalWrite(51, HIGH);
+    digitalWrite(49, HIGH);
+
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(8,  HIGH);
+    digitalWrite(52, LOW);
+    digitalWrite(50, LOW);
+    digitalWrite(48, LOW);
+}
+
+void enableLeft()
+{
+    digitalWrite(13, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(9,  HIGH);
+    digitalWrite(53, LOW);
+    digitalWrite(51, LOW);
+    digitalWrite(49, LOW);
+
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(8,  LOW);
+    digitalWrite(52, HIGH);
+    digitalWrite(50, HIGH);
+    digitalWrite(48, HIGH);
+}
+void enableforward()
+{
+    digitalWrite(13, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9,  LOW);
+    digitalWrite(53, LOW);
+    digitalWrite(51, LOW);
+    digitalWrite(49, LOW);
+
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(8,  HIGH);
+    digitalWrite(52, HIGH);
+    digitalWrite(50, HIGH);
+    digitalWrite(48, HIGH);
+}
+
+void enablereverse()
+{
+    digitalWrite(13, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(9,  HIGH);
+    digitalWrite(53, HIGH);
+    digitalWrite(51, HIGH);
+    digitalWrite(49, HIGH);
+
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(8,  LOW);
+    digitalWrite(52, LOW);
+    digitalWrite(50, LOW);
+    digitalWrite(48, LOW);
+}
