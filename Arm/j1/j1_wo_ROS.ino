9bbf363f65ca36d6739023c4fbd8f828fb71237c
DenizUgur <deniz343@gmail.com>
2020-01-17 16:21:14 +0300

    update j1

--- Arm/j1/j1_wo_ROS.ino
+++ Arm/j1/j1_wo_ROS.ino
@@ -6,16 +6,16 @@
   the readings coming from the encoder will not be correct.
 */
 
-#include <DIO2.h>
 #include <PID_v1.h>
 #include <avr/wdt.h>
 #include "EEPROMAnything.h"
+#include <Encoder.h>
 
 #define GPIO2_PREFER_SPEED 1
 #define c_EncoderPinA 2
 #define c_EncoderPinB 4
-#define EncoderIsReversed
 
+Encoder enc(2, 4);
 
 #define gearRatio 156 
 
@@ -26,22 +26,22 @@ double angle1 = 1.5708; // angle of the motor wrt to the initial position
 
 //PID Library Setup
 //PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, Direction)
-double PID_PWM; //Input that will be sent to the motor as PWM
-double GoalPosition = (90.0 + 45.0) / (180/PI); //The angle to be reached
+double PID_V; //Input that will be sent to the motor as PWM
+double GoalPosition = (90.0+0.0) / (180/PI); //The angle to be reached
 
 //Timer
 long oldTime = 0;
 
 //6.65 0723 0.51
-PID armPID (&angle1, &PID_PWM, &GoalPosition,600.0,3,0.01, DIRECT); //angle1 is encoder reading
+PID armPID (&angle1, &PID_V, &GoalPosition,120,0,0, DIRECT); //angle1 is encoder reading
 
 void setup() {
   //Encoder Settings
   pinMode(c_EncoderPinA, INPUT);      // sets pin A as input
   digitalWrite(c_EncoderPinA, LOW);  // turn on pullup resistors
-  pinMode2(c_EncoderPinB, INPUT);      // sets pin B as input
+  pinMode(c_EncoderPinB, INPUT);      // sets pin B as input
   digitalWrite(c_EncoderPinB, LOW);  // turn on pullup resistors
-  attachInterrupt(0, MotorInterruptA, RISING); //assignment of the interrupt pin
+  //attachInterrupt(0, MotorInterruptA, RISING); //assignment of the interrupt pin
   
   //Motor settings
   pinMode(6, OUTPUT); 
@@ -57,7 +57,7 @@ void setup() {
   //digitalWrite(13,LOW);
   
   //Enable PID
-  armPID.SetOutputLimits(-255,255);
+  armPID.SetOutputLimits(-12,12);
   armPID.SetMode(AUTOMATIC);
 
   Serial.begin(115200);
@@ -66,8 +66,16 @@ void setup() {
   
   //Start timer and restore Encoder
   EEPROM_readAnything(0, _EncoderTicks);
-  angle1 = 1.5708 + (1.0/(500.0*156.0*7.5))*20.043*_EncoderTicks;
-  //EEPROM_readAnything(0, _EncoderTicks);
+  enc.write(_EncoderTicks);
+  angle1 = 1.5708 + (1.0/(500.0*156.0*7.5))*5.49*_EncoderTicks;
+  /*EEPROM.write(0, 0);
+  EEPROM.write(1, 0);
+  EEPROM.write(2, 0);
+  EEPROM.write(3, 0);
+  EEPROM.write(4, 0);
+  EEPROM.write(5, 0);
+  EEPROM.write(6, 0);
+  EEPROM_readAnything(0, _EncoderTicks);*/
   oldTime = millis();
 }
 
@@ -85,41 +93,36 @@ void loop() {
   
   //CCW(PID_PWM);
   if(abs(GoalPosition-angle1) <= 0.01){
-    PID_PWM=0;
+    PID_V=0;
   }
-  //PID_PWM = 55;
-  if(PID_PWM > 0) CW(abs(PID_PWM));
-  else if(PID_PWM < 0) CCW(abs(PID_PWM));
+  //PID_V = -5;
+  int PID_PWM = int(255.0 * pow(PID_V/12.0, 2));
+  if(PID_V > 0) CW(abs(PID_PWM));
+  else if(PID_V < 0) CCW(abs(PID_PWM));
   else halt();
 
-  Serial.print(GoalPosition * 180/PI);
+  /*Serial.print(GoalPosition * 180/PI);
   Serial.print(" ");
   Serial.print(angle1 * 180/PI);
   Serial.print(" ");
-  Serial.println(PID_PWM);
-  //Serial.print(" ");
-  //Serial.println(PID_PWM);
+  Serial.print(PID_V);
+  Serial.print(" ");
+  Serial.println(PID_PWM);*/
+  Serial.print(_EncoderTicks);
+  Serial.print(" ");
+  Serial.println(angle1 * 180/PI);
 
   if (millis() - oldTime > 1000) {
     EEPROM_writeAnything(0, _EncoderTicks);
     oldTime = millis();
   }
-  
-  delay(5);
-}
 
-
-//This method reads the changes in the encoder and updates _EncoderTicks, direction1 and angle1 count
-void MotorInterruptA() {
-  _EncoderBSet = digitalRead2(c_EncoderPinB);   
-  if(digitalRead2(c_EncoderPinB) == HIGH) {
-    _EncoderTicks++;
-  } else if(digitalRead2(c_EncoderPinB) == LOW) {
-    _EncoderTicks--;
-  } 
+  _EncoderTicks = enc.read();
   // Coefficient -> [1/cpt]*3.14*7.5(reduktor)*[motor gear ratio]
   //cpt: counts per turn 500 for HEDL 5540 A02
-  angle1 = 1.5708 + (1.0/(500.0*156.0*7.5))*20.043*_EncoderTicks;
+  angle1 = 1.5708 + (1.0/(500.0*156.0*7.5))*5.49*_EncoderTicks;
+  
+  //delay(5);
 }
 
 //This method sets the motor to turn in clockwise direction
