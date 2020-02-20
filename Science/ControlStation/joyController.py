35d1dbb157ed63e55a229fd545e266dea466d90c
atahancaldir <45735539+atahancaldir@users.noreply.github.com>
2020-02-20 22:17:41 +0300

    Add files via upload

--- Science/ControlStation/joyController.py
+++ Science/ControlStation/joyController.py
@@ -4,12 +4,11 @@ from std_msgs.msg import Float32
 from sensor_msgs.msg import Joy
 
 drillOpen = 0
-shuffle = 3
 
 def joyCallback(data):
-    global drillOpen, shuffle
+    global drillOpen
 
-    message = Int32
+    message = Int32()
 
     if data.buttons[8] == 1:
 
@@ -47,35 +46,31 @@ def joyCallback(data):
         
         elif data.axes[6] == 1:
             message.data = -1
-            pubStep.publish(message) #left #What if this message receive while shuffling?
+            pubStep.publish(message) #left
 
         elif data.buttons[6] == 1:
-            if shuffle == 2:
-                suhffle = 3 #off
-            else:
-                suhffle = 2 #on
-            message.data = shuffle
-            pubStep.publish(message)
+            message.data = 2
+            pubStep.publish(message) #suffle
 
         #water pump 1
         elif data.buttons[3] == 1:
             message.data = 1
-            pubW1.publish(message)
+            pubW.publish(message)
         
         #water pump 2
         elif data.buttons[1] == 1:
-            message.data = 1
-            pubW2.publish(message)
+            message.data = 2
+            pubW.publish(message)
         
         #water pump 3
         elif data.buttons[0] == 1:
-            message.data = 1
-            pubW3.publish(message)
+            message.data = 3
+            pubW.publish(message)
 
         #water pump 4
         elif data.buttons[2] == 1:
-            message.data = 1
-            pubW4.publish(message)
+            message.data = 4
+            pubW.publish(message)
 
         else:
             message.data = 0
@@ -97,11 +92,8 @@ def start():
 
     pubDrillAct = rospy.Publisher("drillActuator", Int32, queue_size=5)
     pubDrill = rospy.Publisher("drill", Int32, queue_size=5)
-    pubW1 = rospy.Publisher("water1", Int32, queue_size=5)
-    pubW2 = rospy.Publisher("water2", Int32, queue_size=5)
-    pubW3 = rospy.Publisher("water3", Int32, queue_size=5)
-    pubW4 = rospy.Publisher("water4", Int32, queue_size=5)
-    pubAct = rospy.Publisher("sensorActuator", Int32, queue_size=5)
+    pubW = rospy.Publisher("water", Int32, queue_size=5)
+    pubSensorAct = rospy.Publisher("sensorActuator", Int32, queue_size=5)
     pubStep = rospy.Publisher("stepper", Int32, queue_size=5)
 
     rospy.Subscriber("joy", Joy, joyCallback)
