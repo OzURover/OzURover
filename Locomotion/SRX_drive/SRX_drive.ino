2d99bd059a2d3719c4f7aa7fa2e7cc4c9b11c206
DenizUgur <deniz343@gmail.com>
2020-02-27 14:18:19 +0300

    update

--- Locomotion/SRX_drive/SRX_drive.ino
+++ Locomotion/SRX_drive/SRX_drive.ino
@@ -14,6 +14,9 @@
 	* forward_high 74
 	*/
 
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
 #define LEFT 7
 #define RIGHT 8
 
@@ -22,14 +25,26 @@
 #define RL 46
 #define RH 20
 
-#define LOWB 0
-#define HIGHB 255
+#define LOWB 0.0
+#define HIGHB 1.0
+
+double goal;
+
+void msgCb(const std_msgs::Flaot32& data) {
+  goal = data.data;
+}
+
+ros::NodeHandle nh;
+ros::Subscriber<std_msgs::Float32> sub("loco", &msgCb);
 
 void setup()
 {
 	pinMode(LEFT, OUTPUT);
 	pinMode(RIGHT, OUTPUT);
 	TCCR4B = TCCR4B & B11111000 | B00000100;
+
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
 
 	// Wait for init and tell SRX's that we are sending PWM
 	delay(1500);
@@ -42,51 +57,109 @@ void setup()
 
 void loop()
 {
-	if (1) {
+  if (goal < 10) {
+    if (goal > 0) {
+      forward(goal);
+    } else {
+      reverse(abs(goal));
+    }
+  } else {
+    if (goal-20 < 0) {
+      right(abs(goal-20));
+    } else {
+      left(goal-20);
+    }
+  }
+  nh.spinOnce();
+  /*
+	if (1)
+	{
 		// Demo1: Show each movement
-		forward(255);
-		delay(2000);
-		reverse(255);
-		delay(2000);
-		left(255);
-		delay(2000);
-		right(255);
-		delay(2000);
-	} else {
+		for (int i = 0; i < 255; i++)
+		{
+			forward(i);
+			delay(20);
+		}
+		for (int i = 255; i > 0; i--)
+		{
+			forward(i);
+			delay(20);
+		}
+		delay(3000);
+		for (int i = 0; i < 255; i++)
+		{
+			reverse(i);
+			delay(20);
+		}
+		for (int i = 255; i > 0; i--)
+		{
+			reverse(i);
+			delay(20);
+		}
+		delay(3000);
+		for (int i = 0; i < 255; i++)
+		{
+			left(i);
+			delay(20);
+		}
+		for (int i = 255; i > 0; i--)
+		{
+			left(i);
+			delay(20);
+		}
+		delay(3000);
+		for (int i = 0; i < 255; i++)
+		{
+			right(i);
+			delay(20);
+		}
+		for (int i = 255; i > 0; i--)
+		{
+			right(i);
+			delay(20);
+		}
+		delay(3000);
+	}
+	else
+	{
 		//Demo2: gradually increase and lower forward speed
 		int l = 48;
 		int r = 46;
 		for (; l < 74 && r > 20;)
 		{
 			analogWrite(RIGHT, r--); //35-70
-			analogWrite(LEFT, l++); //35-70
+			analogWrite(LEFT, l++);  //35-70
 			delay(300);
 		}
 		for (; l > 48 && r < 46;)
 		{
 			analogWrite(RIGHT, r++); //35-70
-			analogWrite(LEFT, l--); //35-70
+			analogWrite(LEFT, l--);  //35-70
 			delay(300);
 		}
-	}
+	}*/
 }
 
-void forward(float pwr) {
+void forward(float pwr)
+{
 	analogWrite(LEFT, int(map(pwr, LOWB, HIGHB, FL, FH)));
 	analogWrite(RIGHT, int(map(pwr, LOWB, HIGHB, RL, RH)));
 }
 
-void reverse(float pwr) {
+void reverse(float pwr)
+{
 	analogWrite(LEFT, int(map(pwr, LOWB, HIGHB, RL, RH)));
 	analogWrite(RIGHT, int(map(pwr, LOWB, HIGHB, FL, FH)));
 }
 
-void left(float pwr) {
+void right(float pwr)
+{
 	analogWrite(LEFT, int(map(pwr, LOWB, HIGHB, FL, FH)));
 	analogWrite(RIGHT, int(map(pwr, LOWB, HIGHB, FL, FH)));
 }
 
-void right(float pwr) {
+void left(float pwr)
+{
 	analogWrite(LEFT, int(map(pwr, LOWB, HIGHB, RL, RH)));
 	analogWrite(RIGHT, int(map(pwr, LOWB, HIGHB, RL, RH)));
 }
