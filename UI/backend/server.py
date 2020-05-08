9483a5d41543a5311a9dca186118a51d0540dfe6
DenizUgur <deniz343@gmail.com>
2020-05-09 02:29:21 +0300

    update

--- UI/backend/server.py
+++ UI/backend/server.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+"""
+Purpose: Bridge between React app and ROS
 
-app = Flask(
-    __name__,
-    static_folder="../rover-ui/build/static",
-    template_folder="../rover-ui/build",
-)
+        #### CONNECTION SUMMARY ####
+
+        Navigation Stack: TCP + ROS
+         Manual Traverse: WebRTC + ROS
+Robotic Arm Manipulation: WebRTC + ROS
+
+Useful Links:
+Python WebRTC Example: https://github.com/aiortc/aiortc/tree/master/examples/server
+"""
+from flask import Flask
+
+app = Flask(__name__, static_folder="../rover-ui/build", static_url_path="/")
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return app.send_static_file("index.html")
 
 
 app.run(debug=True)
