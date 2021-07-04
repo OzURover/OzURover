12573a3027801af824ca189f1f8096316a4c3f0c
Smark2019 <salih8461921@outlook.com>
2021-07-04 16:51:55 +0300

    Some new features added, also still in development process.

--- Science/ph_forecasting.py
+++ Science/ph_forecasting.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+import sys
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import rospy
 from rospy.exceptions import ROSException
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 
 def graph(formula, x_range,percent_of_Red):
@@ -23,10 +25,21 @@ def graph(formula, x_range,percent_of_Red):
 
 
 def image_processor(Image):
-   # image_bgr = cv2.imread('/home/salih/Desktop/Mars.jpg', cv2.IMREAD_COLOR)
-    image_bgr = cv2.imread(Image, cv2.IMREAD_COLOR)
-    channels = cv2.mean(image_bgr)
 
+    # Converting from Image to open cv image type:
+    bridge = CvBridge()
+
+    try:
+      img_cv2 = bridge.imgmsg_to_cv2(Image, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    # image wrapping
+    img_wrapped = warpImg(img_cv2,valTrackbars(),1000,800) 
+    # image_bgr = cv2.imread('/home/salih/Desktop/Mars.jpg', cv2.IMREAD_COLOR)
+
+
+    channels = cv2.mean(img_wrapped)
     # Swap blue and red values (making it RGB, not BGR)
     observation = np.array([(channels[2], channels[1], channels[0])])
     print(observation)
@@ -46,12 +59,38 @@ def image_processor(Image):
     #graph('0.0956*x+4.2722', range(0, 100),percent_of_Red)
 
     rospy.loginfo("Predicted pH: "+ str(0.0956 * percent_of_Red + 4.2722 ))
-
+   
+    # Text Settings:
+        # font
+    font = cv2.FONT_HERSHEY_SIMPLEX
+      
+    # org
+    org = (250, 500)
+      
+    # fontScale
+    fontScale = 1
+      
+    # Blue color in BGR
+    color = (255, 0, 0)
+      
+    # Line thickness of 2 px
+    thickness = 2
+      
+    # Using cv2.putText() method
+    image = cv2.putText(img_cv2, 'Predicted pH: '+str(0.0956 * percent_of_Red + 4.2722), org, font, 
+                      fontScale, color, thickness, cv2.LINE_AA)
+    
+    img_cv2 = drawPoints(img_cv2,valTrackbars())
+    cv2.imshow("Image",img_cv2)
+    cv2.waitKey(1)
+    
+		
+      
 
 def main():
   rospy.init_node('marsyard_pH_forecasting', anonymous=True)
   rospy.loginfo("marsyard_pH_forecasting node initiliazed !")
-  image_sub = rospy.Subscriber("/zed2/right_raw/image_raw_color",Image, image_processor)
+  image_sub = rospy.Subscriber("/zed2/right/image_rect_color",Image, image_processor)
   try:
     rospy.spin()
   except:
@@ -64,5 +103,45 @@ def main():
 #plt.show()
 
 
+def warpImg(img, points, w, h, inv=False):
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+    if inv:
+        matrix = cv2.getPerspectiveTransform(pts2, pts1)
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgWarp = cv2.warpPerspective(img, matrix, (w, h))
+    return imgWarp
+
+def nothing():
+  pass
+
+
+def initializeTrackbars(intialTracbarVals, wT=1000, hT=800):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0], wT // 2, nothing)
+    cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], hT, nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2], wT // 2, nothing)
+    cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
+
+
+def valTrackbars(wT=1000, hT=600):
+    widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
+    heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
+    widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
+    heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
+    points = np.float32([(widthTop, heightTop), (wT - widthTop, heightTop),
+                         (widthBottom, heightBottom), (wT - widthBottom, heightBottom)])
+    return points
+
+
+def drawPoints(img, points):
+    for x in range(4):
+        cv2.circle(img, (int(points[x][0]), int(points[x][1])), 15, (0, 0, 255), cv2.FILLED)
+    return img
+
 if __name__ == "__main__":
-    main()
+  initializeTrackbars([153,498,160,670])
+
+  main()
